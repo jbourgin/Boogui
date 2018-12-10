3fa44a988a8c2e4e9ef7040b51a183b13a7a24ff
Alexandre Maréchal <marechalalex@gmail.com>
2018-12-10 11:26:24 +0100

    Improving video player

--- gui/video_widget.py
+++ gui/video_widget.py
@@ -1,9 +1,9 @@
 import sys
 import time
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QSlider
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, Qt
 
 from eyetracking.utils import *
 from gui.utils import *
@@ -12,7 +12,19 @@ class VideoWidget(QWidget):
 
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.vid_wid = QVideoWidget()
+
         self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.media_player.stateChanged.connect(self.mediaStateChanged)
+        self.media_player.positionChanged.connect(self.positionChanged)
+        self.media_player.durationChanged.connect(self.durationChanged)
+        self.media_player.error.connect(self.handleError)
+
+        self.position_slider = QSlider(Qt.Horizontal)
+        self.position_slider.setRange(0, 0)
+        self.position_slider.sliderMoved.connect(self.setPosition)
+
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
@@ -27,13 +39,15 @@ class VideoWidget(QWidget):
 
     def setVideo(self, path: str) -> None:
         self.clear()
-        vid_wid = QVideoWidget()
         play_button = QPushButton("Play")
         play_button.clicked.connect(self.play)
         url = joinPaths(getTmpFolder(), path)
+
         self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(url)))
-        self.media_player.setVideoOutput(vid_wid)
-        self.layout.addWidget(vid_wid)
+        self.media_player.setVideoOutput(self.vid_wid)
+
+        self.layout.addWidget(self.vid_wid)
+        self.layout.addWidget(self.position_slider)
         self.layout.addWidget(play_button)
 
     def play(self):
@@ -41,3 +55,24 @@ class VideoWidget(QWidget):
             self.media_player.pause()
         else:
             self.media_player.play()
+
+    def mediaStateChanged(self, state):
+        if self.media_player.state() == QMediaPlayer.PlayingState:
+            self.play_button.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.play_button.setIcon(
+                    self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.position_slider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.position_slider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.media_player.setPosition(position)
+
+    def handleError(self):
+        self.play_button.setEnabled(False)
+        self.errorLabel.setText("Error: " + self.media_player.errorString())
