ec7618e7b1643a850f3082422cedbea157829118
Jessica Bourgin <jessica.bourgin@gmail.com>
2023-05-03 08:35:21 +0200

    add dark theme

--- test_gui.py
+++ test_gui.py
@@ -1,9 +1,14 @@
 from PyQt5.QtWidgets import QApplication
 from gui.gui import *
+# https://pypi.org/project/pyqtdarktheme/
+import qdarktheme
+
 
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
+    # Apply dark theme to app.
+    qdarktheme.setup_theme()
 
     mainWindow = Main()
     sys.exit(app.exec_())
