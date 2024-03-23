1249139db48e08b615094451d1f932118b40fd39
Jessica Bourgin <jessica.bourgin@gmail.com>
2024-03-23 06:50:33 +0100

    final version of Hemifield

--- experiments/Hemifield.py
+++ experiments/Hemifield.py
@@ -1,6 +1,14 @@
 from eyetracking.experiment import *
 from eyetracking.interest_region import *
 from PyQt5.QtWidgets import QApplication
+import pandas as pd
+
+class HemiCol(Col):
+    CORRECTSTART = "Correct start trial"
+    SACCADEDURING = "Saccade during face presentation"
+    SACCADETOWARD = "Saccade toward face during presentation"
+    LANDINGDURING = "Landing on face during presentation"
+    AGESTIM = "Age"
 
 class Exp(Experiment):
 
@@ -17,6 +25,8 @@ class Exp(Experiment):
         # frames
         self.left = RectangleRegion((self.screen_center[0]/2, self.screen_center[1]), self.half_width, self.half_height)
         self.right = RectangleRegion((self.screen_center[0]*1.5, self.screen_center[1]), self.half_width, self.half_height)
+
+        self.psychopyVariables = ["StimName", "Emotion", "Age", "StimPos", "Response", "ResponseTime"]
 
 
     ###############################################
@@ -38,7 +48,7 @@ class Exp(Experiment):
         if len(line) >= 3 and line[2] == 'TRIALID':
             try:
                 time = int(line[1])
-                trial_number = int(line[3].split("/")[0])
+                trial_number = int(line[3].split("/")[0])-1 # 1 in Boogui, 0 in psychopy
                 if int(line[3].split("/")[1]) != 8:# training
                     stimulus = line[4]
                     return StartTrial(time, trial_number, stimulus)
@@ -63,16 +73,38 @@ class Exp(Experiment):
 
     def getTrialRegions(self, trial):
         # WARNING : we don't know where target was
-        return [self.left, self.right]
+        if trial.features["StimPos"] == 380:
+            return [self.right]
+        elif trial.features["StimPos"] == -380:
+            return [self.left]
+        else:
+            raise ExperimentException("No valid StimPos {0} for trial {1}".format(trial.features["StimPos"], trial.id))
 
     ######################################################
     ############## End of Overriden methods ##############
     ######################################################
 
+    # WARNING : hardcoded specifically since we forgot to include side picture was presented in eyelink file
+    def getCSVFilePath(self, subject):
+        dataPath = "C:/Users/jessi/Ingé/Programmation/Pascal/2023-2024/230927 Tâche eyetracking hémichamp/Données eyetracking/data"
+        csvFiles = [file for file in os.listdir(dataPath) if os.path.splitext(file)[1] == ".csv"]
+        subjFiles = [file for file in csvFiles if file.startswith("{0}{1}_".format("o" if subject.group == "OLD" else "y", subject.id))]
+        if len(subjFiles) != 1:
+            raise ExperimentException('{0} files found for subject {1}'.format(len(subjFiles), subject.id))
+        return os.path.join(dataPath, subjFiles[0])
+
+    def setCSVVariables(self, subject, trial):
+        csvFilePath = self.getCSVFilePath(subject)
+        df = pd.read_csv(csvFilePath)
+        for row_idx in range(len(df)):
+            if df.loc[row_idx, "trials.thisN"] == trial.id:
+                for variable in self.psychopyVariables:
+                    trial.features[variable] = df.loc[row_idx, variable]
+
     def processTrial(self, subject: Subject, trial):
         super().processTrial(subject, trial)
-        targetName = trial.getStimulus()
 
+        trial_var = self.setCSVVariables(subject, trial)
         regions = self.getTrialRegions(trial)
 
         region_fixations = trial.getFixationTime(InterestRegionList(regions))
@@ -82,43 +114,36 @@ class Exp(Experiment):
             blink_category = BLINK.NO
         else:
             if trial.blinks[0].getStartTime() < trial.features["image_end"]:
-                blink_category = BLINK.EARLY
+                blink_category = ERROR.EARLY_BLINK
             else:
                 blink_category = BLINK.LATE
 
-        first_sac_to_target = False
+        sac_to_target = False
         landing_on_target = False
         if len(trial.saccades) > 0 and len(region_fixations) > 0:
             for saccade in trial.saccades:
                 # Saccade must be before first region fixation
-                # WARNING : we should select only right or left side depending on side where image was presented
                 if saccade.getStartTime() < region_fixations[0].getStartTime():
                     # start of saccade should occur before end of image presentation
                     if saccade.getStartTime() < trial.features["image_end"]:
-                        first_sac_to_target = True
+                        sac_to_target = True
                     else:
-                        first_sac_to_target = False
+                        sac_to_target = False
             if region_fixations[0].getStartTime() < trial.features["image_end"]:
                 landing_on_target = True
 
-        # Error :
-        if not trial.isStartValid(self.screen_center, self.valid_distance_center)[0]:
-            error = ERROR.START_INVALID
-        elif blink_category == BLINK.EARLY:
-            error = ERROR.EARLY_BLINK
-        elif landing_on_target:
-            error = "Landing on target"
-        elif first_sac_to_target:
-            error = "Saccade toward target"
-        elif len(trial.saccades) > 0 and trial.saccades[0].getStartTime() < trial.features["image_end"]:
-            error = ERROR.EARLY_SACCADE
-        else:
-            error = '0'
-
         # Writing data in result csv file
         new_dict = {
-            Col.TARGET: targetName,
-            Col.ERR: error,
+            Col.TARGET: trial.features["StimName"],
+            Col.EMOTION: trial.features["Emotion"],
+            HemiCol.AGESTIM: trial.features["Age"],
+            Col.TARGET_POS: "Left" if trial.features["StimPos"] == -380 else "Right",
+            Col.RESP: trial.features["Response"],
+            Col.RT: trial.features['ResponseTime'],
+            HemiCol.CORRECTSTART: trial.isStartValid(self.screen_center, self.valid_distance_center)[0],
+            HemiCol.SACCADEDURING: len(trial.saccades) > 0 and trial.saccades[0].getStartTime() < trial.features["image_end"],
+            HemiCol.SACCADETOWARD: sac_to_target,
+            HemiCol.LANDINGDURING: landing_on_target,
             Col.BLINK: blink_category,
         }
 
